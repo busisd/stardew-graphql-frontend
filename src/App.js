a828900487a396e8b6c411d969234e46aaeac23e
Daniel Busis <busis.daniel@gmail.com>
2022-02-05 20:55:11 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import { DataDisplay, DataDisplay2 } from './DataDisplay';
+import "./App.css";
+import { DataDisplay } from "./DataDisplay";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Data fetched from a GraphQL API</p>
         <DataDisplay />
-        <DataDisplay2 />
       </header>
     </div>
   );
--- src/DataDisplay.jsx
+++ src/DataDisplay.jsx
@@ -1,59 +1,25 @@
 import { gql, useQuery } from "@apollo/client";
-import { useState } from "react";
+import { useMemo } from "react";
 
-const BOOK_QUERY = `
+const BOOK_QUERY = gql`
   query {
     books {
-      author,
       title
-    }
-  }
-`;
-
-const BOOK_QUERY_2 = gql`
-  query {
-    books {
       author
-      title
+      color
     }
   }
 `;
-
-const getData = async () => {
-  return fetch("http://localhost:4000/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      query: BOOK_QUERY,
-    }),
-  }).then((data) => {
-    return data.json();
-  });
-};
 
 export const DataDisplay = () => {
-  const [data, setData] = useState("Press the button to load data");
-
-  console.log("1", BOOK_QUERY);
-  console.log("2", BOOK_QUERY_2);
-
-  return (
-    <>
-      <button
-        onClick={async () => {
-          setData(await getData());
-        }}
-      >
-        Load Data
-      </button>
-      <textarea readOnly className="data-display" value={JSON.stringify(data, null, 2)}></textarea>
-    </>
-  );
-};
-
-export const DataDisplay2 = () => {
-  const { loading, error, data } = useQuery(BOOK_QUERY_2);
+  const { loading, error, data } = useQuery(BOOK_QUERY);
   console.log({ loading, error, data });
 
-  return <textarea readOnly className="data-display" value={JSON.stringify(data, null, 2)}></textarea>;
+  const dataDisplayContent = useMemo(() => {
+    if (loading) return "Loading...";
+    if (error) return error.toString();
+    return JSON.stringify(data, null, 2);
+  }, [loading, error, data]);
+
+  return <textarea readOnly className="data-display" value={dataDisplayContent}></textarea>;
 };
